e7c160af63f58358463c9463febd8cb2b7ee0ce7
Matheus <matheusrcrv07@gmail.com>
2022-09-01 14:22:19 -0300

    entendendo as diferenças e uso de imagem em public e em src

--- avancando-no-react/src/App.js
+++ avancando-no-react/src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
 
+import ImgAssets from './assets/img-assets.jpg'
+
 function App() {
   return (
     <div className="App">
+      <h1>Seção 3 - Avançando em React</h1>
+      {/* Imagem dentro de Public */}
+      <img src="./img-public.jpg" alt="Centro da cidade" />
+      {/* Imagem dentro de Assets */}
+      <img src={ImgAssets} alt="Carro na estrada" />
     </div>
   );
 }
