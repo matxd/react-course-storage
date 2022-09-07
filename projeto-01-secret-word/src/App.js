09972af1eb1460c546d377aa9424f049e59e0caa
Matheus <matheusrcrv07@gmail.com>
2022-09-07 14:08:02 -0300

    Finalizado projeto 01 Secret Word

--- projeto-01-secret-word/src/App.js
+++ projeto-01-secret-word/src/App.js
@@ -1,11 +1,106 @@
 import './App.css';
 
+import { useCallback, useEffect, useState } from 'react';
+
+import { wordsList } from './data/words';
+
+import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+const stages = [
+  {id: 1, name: "start"},
+  {id: 2, name: "game"},
+  {id: 3, name: "end"},
+]
+
+const guessesQty = 5
+
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name)
+  const [words] = useState(wordsList)
+  const [pickedWord, setPickedWord] = useState("")
+  const [pickedCategory, setPickedCategory] = useState("")
+  const [letters, setLetters] = useState([])
+  const [guessedLetters, setGuessedLetters] = useState([])
+  const [wrongLetters, setWrongLetters] = useState([])
+  const [guesses, setGuesses] = useState(guessesQty)
+  const [score, setScore] = useState(0)
+
+  const pickWordAndCategory = useCallback(() => {
+    const randomCategory = Object.keys(words)
+    const category = randomCategory[Math.floor(Math.random() * Object.keys(randomCategory).length)]
+    const word = words[category][Math.floor(Math.random() * words[category].length)]
+
+    return {category, word}
+  }, [words])
+
+  const startGame = useCallback(() => {
+    clearLetterStates()
+    const { category, word } = pickWordAndCategory()
+    let wordLetters = word.split("").map((letter) => letter.toLowerCase())
+
+    setPickedWord(word)
+    setPickedCategory(category)
+    setLetters(wordLetters)
+    setGameStage(stages[1].name)
+  }, [pickWordAndCategory])
+
+  const verifyLetter = (letter) => {
+    const normalizedLetter = letter.toLowerCase()
+
+    if(guessedLetters.includes(normalizedLetter) || wrongLetters.includes(normalizedLetter)){
+      return
+    }
+
+    if(letters.includes(normalizedLetter)){
+      setGuessedLetters((actualGuessedLetters) => [
+        ...actualGuessedLetters,
+        normalizedLetter
+      ])
+    } else {
+      setWrongLetters((actualWrongLetters) => [
+        ...actualWrongLetters,
+        normalizedLetter
+      ])
+      setGuesses((actualGuesses) => actualGuesses - 1)
+    }
+  }
+
+  const clearLetterStates = () => {
+    setGuessedLetters([])
+    setWrongLetters([])
+  }
+
+  useEffect(() => {
+    if(guesses <= 0){
+      clearLetterStates()
+      setGameStage(stages[2].name)
+    }
+  }, [guesses])
+
+  useEffect(() => {
+    const uniqueLetters = [...new Set(letters)]
+    
+    if(guessedLetters.length === uniqueLetters.length){
+      setScore((actualScore) => actualScore += 100)
+      startGame()
+    }
+  }, [guessedLetters, letters, startGame])
+
+  const retryGame = () => {
+    setScore(0)
+    setGuesses(guessesQty)
+    setGameStage(stages[0].name)
+  }
+
   return (
     <div className="App">
-
+      {gameStage === "start" && <StartScreen startGame={startGame} />}
+      {gameStage === "game" && <Game verifyLetter={verifyLetter} pickedWord={pickedWord} pickedCategory={pickedCategory} letters={letters} guessedLetters={guessedLetters} wrongLetters={wrongLetters} guesses={guesses} score={score} />}
+      {gameStage === "end" && <GameOver retryGame={retryGame} score={score} />}
     </div>
   );
-}
+};
 
 export default App;
---
+++ projeto-01-secret-word/src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+  Carro: ["Motor", "Porta", "Capô", "Pneu", "Volante"],
+  Fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja"],
+  Corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
+  Computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "Fonte"],
+  Programação: ["Linguagem", "Framework", "JavaScript", "React", "TypeScript"],
+  Alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo", "Pizza"],
+};
