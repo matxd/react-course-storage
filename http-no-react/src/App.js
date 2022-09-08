c6b9398f693ba32af2e7d5a8bae64886d5e23a6f
Matheus <matheusrcrv07@gmail.com>
2022-09-08 18:25:47 -0300

    Consumindo API, enviando dados com forms e exibindo de forma dinâmica

--- http-no-react/src/App.js
+++ http-no-react/src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useState, useEffect } from 'react';
+
+const url = "http://localhost:3000/products"
+
 function App() {
+  const [products, setProducts] = useState([])
+  const [name, setName] = useState("")
+  const [price, setPrice] = useState("")
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const product = {
+      name,
+      price,
+    }
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(product)
+    })
+
+    const addedProduct = await response.json()
+    setProducts((prevProducts) => [ ...prevProducts, addedProduct ])
+
+    setName("")
+    setPrice("")
+  }
+
+  useEffect(() => {
+    async function fetchData(){
+      const response = await fetch(url)
+      const data = await response.json()
+      setProducts(data)
+    }
+
+    fetchData()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Seção 07 - HTTP</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.name} - R$ {product.price}</li>
+        ))}
+      </ul>
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome: <input type="text" value={name} name="name" onChange={(e) => setName(e.target.value)} required />
+            Price: <input type="number" value={price} name="price" onChange={(e) => setPrice(e.target.value)} required />
+          </label>
+          <input type="submit" value="Enviar formulario" />
+        </form>
+      </div>
     </div>
   );
 }
